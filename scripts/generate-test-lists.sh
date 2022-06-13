45c1c3fef77d097fe41042447931183f8b9f112b
Flynn Hembroff <flynnhembroff@gmail.com>
2022-06-13 12:22:39 -0400

    Added --timings-type=classname to config.yml

--- scripts/generate-test-lists.sh
+++ scripts/generate-test-lists.sh
@@ -23,22 +23,56 @@ set -o nounset
 set -o pipefail
 
 # Generate list of bitbucket tests
-grep -rnwl . -e "BitBucketTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " > bitbuckettests.txt
-
+grep -rnwl . -e "BitBucketTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " > temp.txt #bitbuckettests.txt
+> bitbuckettests.txt
+for file in $(cat temp.txt)
+do
+  basename $file |  sed -e "s/.java/,/" >>  bitbuckettests.txt
+done
 # Generate list of non-confidential tests
-grep -rnwl . -e "NonConfidentialTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " > nonconfidentialtests.txt
+grep -rnwl . -e "NonConfidentialTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " > temp.txt
+> nonconfidentialtests.txt
+for file in $(cat temp.txt)
+do
+  basename $file |  sed -e "s/.java/,/" >>  nonconfidentialtests.txt
+done
 
 # Generate list of LanguageParsingTest
-grep -rnwl . -e "LanguageParsingTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " > languageparsingtests.txt
+grep -rnwl . -e "LanguageParsingTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " > temp.txt
+> languageparsingtests.txt
+for file in $(cat temp.txt)
+do
+  basename $file |  sed -e "s/.java/,/" >>  languageparsingtests.txt
+done
 
 # Generate list of unit tests
-grep -rnwl  . -Le "BitBucketTest.class\|LanguageParsingTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " >  unit-tests.txt
+grep -rnwl  . -Le "BitBucketTest.class\|LanguageParsingTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " >  temp.txt
+> unit-tests.txt
+for file in $(cat temp.txt)
+do
+  basename $file |  sed -e "s/.java/,/" >>  unit-tests.txt
+done
 
 # Generate list of workflow tests
-grep -rnwl . -e "WorkflowTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " > workflowtests.txt
+grep -rnwl . -e "WorkflowTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " > temp.txt
+> workflowtests.txt
+for file in $(cat temp.txt)
+do
+  basename $file |  sed -e "s/.java/,/" >>  workflowtests.txt
+done
 
 # Generate list of tool tests
-grep -rnwl . -e "ToolTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " > tooltests.txt
+grep -rnwl . -e "ToolTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " > temp.txt
+> tooltests.txt
+for file in $(cat temp.txt)
+do
+  basename $file |  sed -e "s/.java/,/" >> tooltests.txt
+done
 
 # Generate list of integtration tests
-grep -rnwl  . -Le "ToolTest.class\|WorkflowTest.class\|SlowTest.class\|RegressionTest.class\|NonConfidentialTest.class\|LanguageParsingTest.class\|BitBucketTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " >  integration-tests.txt
+grep -rnwl  . -Le "ToolTest.class\|WorkflowTest.class\|SlowTest.class\|RegressionTest.class\|NonConfidentialTest.class\|LanguageParsingTest.class\|BitBucketTest.class" | grep -E "IT*.java|Test*.java" | grep -v ./dockstore-common/src/main/java/io/dockstore/common/ | tr "\n" " " >  temp.txt
+> integration-tests.txt
+for file in $(cat temp.txt)
+do
+  basename $file |  sed -e "s/.java/,/" >> integration-tests.txt
+done
